e46e4f9661643fcbb42891ac9004205e67356c9b
Mary Kate Saguin <20220025024@my.xu.edu.ph>
2025-02-11 21:44:23 +0800

    added some few changes

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -7,9 +7,9 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white dark:bg-gray-800 overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 text-gray-900 dark:text-gray-100">
-                    {{ __("Welcome to XU User Portal!") }}
+            <div class="bg-black overflow-hidden shadow-sm sm:rounded-lg">
+                <div class="text-center p-6 text-white">
+                    <p>Welcome to<b> XU User Portal 📚</b></p>
                 </div>
             </div>
         </div>
